ac8c5f8b58807c4a3bc050bf0272b5c2572dd1bf
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-04-24 07:40:34 -0700

    width handler

---
+++ 9_width_handler.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * width_handler - Handle the field width for non-custom conversion specifiers
+ * @format: formatted string
+ * @i: list of arguments to print
+ * @args: argument lists
+ *
+ * Return: width
+ */
+
+int width_handler(const char *format, int *i, va_list args)
+{
+	int c_i;
+	int width = 0;
+
+	c_i = *i + 1;
+	while (format[c_i] != '\0')
+	{
+		if (is_digit(format[c_i]))
+		{
+			width *= 10;
+			width += format[c_i] - '0';
+		}
+		else if (format[c_i] == '*')
+		{
+			c_i++;
+			width = va_arg(args, int);
+			break;
+		}
+		else
+			break;
+
+		c_i++;
+	}
+
+	*i = c_i - 1;
+
+	return (width);
+}

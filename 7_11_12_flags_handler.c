2c5c4f46b020ed4bf86d9b11c1e8f1319002a747
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-04-25 23:01:29 +0000

    Update 7_11_12_flags_handler.c

--- 7_11_12_flags_handler.c
+++ 7_11_12_flags_handler.c
@@ -7,25 +7,38 @@
 *
 * Return: the flags
 */
-int flags_handler(const char *format, int *i) {
-    int j; /* to iterate through the bitmask */
-    int current_i; /* keep track of current position in the format string passed*/
-    int flags = 0; /* the possible flag characters for the _printf() function. */
-    const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'}; /* the corresponding flag bitmasks for the flags listed above. */
-    const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
-    for (current_i = *i + 1; format[current_i] != '\0'; current_i++) {
-        for (j = 0; FLAGS_CH[j] != '\0'; j++) {
-            if (format[current_i] == FLAGS_CH[j]) {
-                /* If a flag character is found, set the corresponding flag bitmask. */
-                flags = flags | FLAGS_ARR[j];
-                break;
-            }
+int flags_handler(const char *format, int *i)
+{
+        int j; /* to iterate through the bitmask */
+        int current_i; /* keep track of current position in the format string passed*/
+        int flags = 0;
+
+        /* the possible flag characters for the _printf() function. */
+        const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+
+        /* the corresponding flag bitmasks for the flags listed above. */
+        const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+        for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
+        {
+                for (j = 0; FLAGS_CH[j] != '\0'; j++)
+                {
+                        if (format[current_i] == FLAGS_CH[j])
+                        {
+                                /* If a flag character is found, set the corresponding flag bitmask. */
+                                flags = flags | FLAGS_ARR[j];
+                                break;
+                        }
+                }
+
+                if (FLAGS_CH[j] == '\0')
+                {
+                        /* If an invalid flag character is found, return -1. */
+                        return -1;
+                }
         }
-        if (FLAGS_CH[j] == '\0') {
-            /* If an invalid flag character is found, return an error code. */
-            return (-1);
-        }
-    }
-    *i = current_i - 1;
-    return (flags);
+
+        *i = current_i - 1;
+
+        return (flags);
 }

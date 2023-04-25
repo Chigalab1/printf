cf31339640f018a2aca9b3cbde74fea49d5d456e
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-04-25 22:55:56 +0000

    Update 7_11_12_flags_handler.c

--- 7_11_12_flags_handler.c
+++ 7_11_12_flags_handler.c
@@ -11,8 +11,8 @@ int flags_handler(const char *format, int *i) {
     int j; /* to iterate through the bitmask */
     int current_i; /* keep track of current position in the format string passed*/
     int flags = 0; /* the possible flag characters for the _printf() function. */
-    const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\'', '\0'}; /* the corresponding flag bitmasks for the flags listed above. */
-    const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, F_APOSTROPHE, 0};
+    const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'}; /* the corresponding flag bitmasks for the flags listed above. */
+    const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
     for (current_i = *i + 1; format[current_i] != '\0'; current_i++) {
         for (j = 0; FLAGS_CH[j] != '\0'; j++) {
             if (format[current_i] == FLAGS_CH[j]) {

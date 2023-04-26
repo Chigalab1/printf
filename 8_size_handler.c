575d447ef6aa28da145ed9bec8dacf54279e0fc9
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-04-25 21:45:05 -0700

    size handle

--- 8_size_handler.c
+++ 8_size_handler.c
@@ -8,7 +8,7 @@
  * @format: determine whether a size modifier character (either 'l' or 'h')
  *          is present in the format string
  *
- * @i: list of arguments passed to the funtion to  print
+ * @i: arguments passed to the funtion to  print
  *
  * Return: the size of argument
  */

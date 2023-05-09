6873e9e4d213ca5a17bfefcf31f3a312b4fe4a97
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-09 13:15:31 +0000

    modified print handler

--- printf_handler.c
+++ printf_handler.c
@@ -16,28 +16,26 @@
 int print_handler(const char *fmt, int *ind, va_list args, char buffer[],
 	int flags, int width, int prec, int size)
 {
-	int i, unknown_fmt_len = 0, num_char_print  = 0, ret = 0;
+	int i, unknown_fmt_len = 0, num_char_print  = 0; /*ret = 0;*/
 	fmt_t fmt_types[] = {{'c', print_char}, {'s', print_str}, {'%', print_per},
 		{'R', print_rot13}, {'i', print_integer}, {'d', print_integer},
 		{'b', print_u_bin}, {'u', print_unsignd}, {'o', print_oct}, {'x', print_hex},
 		{'X', print_hex_upper}, {'S', print_non_printable}, {'r', print_str_rev},
 		{'p', print_pointer}, {'\0', NULL}};
 
+
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(args, buffer, flags, width, prec, size));
+
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
+
 		unknown_fmt_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-		{
-			ret = write(1, " ", 1);
-			if (ret < 0)
-				return (-1);
-			unknown_fmt_len += ret;
-		}
+			unknown_fmt_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -47,12 +45,11 @@ int print_handler(const char *fmt, int *ind, va_list args, char buffer[],
 				--(*ind);
 			return (1);
 		}
-		ret = write(1, &fmt[*ind], 1);
-		if (ret < 0)
-			return (-1);
-		unknown_fmt_len += ret;
-		num_char_print += unknown_fmt_len;
-		return (num_char_print);
+		unknown_fmt_len += write(1, &fmt[*ind], 1);
+		return (unknown_fmt_len);
 	}
 	return (num_char_print);
 }
+
+
+
